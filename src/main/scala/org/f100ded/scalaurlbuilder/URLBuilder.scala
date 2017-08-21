4abf6f71d00930eb4f1ce773c87ee9a161a94387
Artem Tartakynov <artem.tad@gmail.com>
2017-08-21 19:21:01 +0700

    major refactoring of the builder

--- src/main/scala/org/f100ded/scalaurlbuilder/URLBuilder.scala
+++ src/main/scala/org/f100ded/scalaurlbuilder/URLBuilder.scala
@@ -1,64 +1,106 @@
 package org.f100ded.scalaurlbuilder
 
-import java.net.{URL, URLDecoder, URLEncoder}
+import java.net.{MalformedURLException, URL, URLDecoder, URLEncoder}
 
 /**
   * Simple way of constructing URLs
   */
-class URLBuilder private(base: String, pathSegments: List[String], queryParams: List[(String, String)]) {
-  final def addPathSegments(segments: String): URLBuilder = segments.split("/", 2).toList match {
-    case Nil => this
-    case head :: tail =>
-      lazy val cleanSegments = if (pathSegments.nonEmpty && pathSegments.last.isEmpty) {
-        pathSegments.dropRight(1)
-      } else {
-        pathSegments
-      }
+class URLBuilder private(base: String,
+                         pathSegments: List[String],
+                         val queryParameters: Option[String],
+                         fragment: Option[String]) {
+  lazy val location: String = (Seq(base) ++ pathSegments.map(encode)).mkString("/") +
+    fragment.map("#" + _).getOrElse("")
 
-      val builder = head match {
-        case s if s == "." => this
-        case s if s == ".." => new URLBuilder(base, cleanSegments.dropRight(1), queryParams)
-        case s => new URLBuilder(base, cleanSegments :+ decode(s), queryParams)
-      }
-
-      tail.foldLeft(builder) {
-        case (acc, v) => acc.addPathSegments(v)
-      }
-  }
-
-  def addQueryParameters(params: (String, String)*): URLBuilder = {
-    new URLBuilder(base, pathSegments, queryParams ++ params.map { case (p, v) => (decode(p), decode(v)) })
-  }
-
-  override def toString: String = {
-    val segments = Option(pathSegments).filter(_.nonEmpty).getOrElse(List("")).map(encode)
-    val location = (base +: segments).mkString("/")
-    val parameters = queryParams.map { case (p, v) => s"${encode(p)}=${encode(v)}" }.mkString("&")
-    if (parameters.isEmpty) {
-      location
-    } else {
-      s"$location?$parameters"
+  /**
+    * Adds given segments and returns a new builder with the added segments.
+    * The segments can be separated by a slash (/) or represented as separate values in the given sequence of segments.
+    *
+    * @param segments Segments added to the builder
+    * @return A new builder created from the current one with added segments
+    */
+  def withPathSegments(segments: String*): URLBuilder = {
+    val flatSegments = segments.flatMap(_.split("/", Integer.MAX_VALUE))
+    flatSegments.foldLeft(this) {
+      case (builder, segment) => builder.withSegment(segment)
     }
+  }
+
+  /**
+    * Adds given query parameters and returns a new builder with the added parameters.
+    *
+    * @param params Query string parameters added to the builder
+    * @return A new builder created from the current one with added query parameters
+    */
+  def withQueryParameters(params: (String, String)*): URLBuilder = {
+    val render = normalize(_: String) + "=" + normalize(_: String)
+    val newQueryParameters = Seq(queryParameters.toSeq, params.map(render.tupled))
+      .flatten
+      .filter(_.length > 1)
+      .mkString("&")
+
+    new URLBuilder(base, pathSegments, Some(newQueryParameters), fragment)
+  }
+
+  /**
+    * A new builder containing updating this builder's URL fragment with the given fragment
+    * @param newFragment A new fragment
+    * @return A new builder with the fragment
+    */
+  def withFragment(newFragment: Option[String]): URLBuilder = {
+    new URLBuilder(base, pathSegments, queryParameters, newFragment.map(f => normalize(f.stripPrefix("#"))))
+  }
+
+  /**
+    * Builds a string representing the constructed URL
+    *
+    * @return A string representation of the URL constructed by the current builder
+    */
+  override def toString: String = queryParameters match {
+    case Some(params) => s"$location?$params"
+    case None => location
+  }
+
+  /**
+    * Builds the constructed URL
+    *
+    * @return The URL constructed by the current builder
+    */
+  def toURL: URL = new URL(toString)
+
+  private def withSegment(segment: String): URLBuilder = segment match {
+    case s if s == "." => this
+    case s if s == ".." => new URLBuilder(base, pathSegments.dropRight(1), queryParameters, fragment)
+    case s => new URLBuilder(base, pathSegments.filter(_.nonEmpty) :+ decode(s), queryParameters, fragment)
   }
 
   private def decode(value: String): String = URLDecoder.decode(value, "UTF-8")
 
   private def encode(value: String): String = URLEncoder.encode(value, "UTF-8")
+
+  private def normalize(s: String): String = encode(decode(s))
 }
 
 object URLBuilder {
-  def apply(base: URL): URLBuilder = {
-    val parameters = Option(base.getQuery).toSeq.flatMap {
-      _.split('&').flatMap(x => x.split("=", 2).toList match {
-        case a :: b :: Nil => Some(a -> b)
-        case a :: Nil => Some(a -> "")
-        case Nil => None
-      })
-    }
-    new URLBuilder(s"${base.getProtocol}://${base.getAuthority}", List(), List())
-      .addPathSegments(base.getPath)
-      .addQueryParameters(parameters: _*)
-  }
+  /**
+    * Returns a new URLBuilder with the given base URL
+    *
+    * @param base Base URL
+    * @return A new URLBuilder instance
+    */
+  def apply(base: URL): URLBuilder = new URLBuilder(
+    base = s"${base.getProtocol}://${base.getAuthority}",
+    pathSegments = List(),
+    queryParameters = Option(base.getQuery),
+    fragment = Option(base.getRef)
+  ).withPathSegments(base.getPath)
 
+  /**
+    * Returns a new URLBuilder with the given base URL
+    *
+    * @throws MalformedURLException if the given base is not a valid URL
+    * @param base String representation of the base URL
+    * @return A new URLBuilder instance
+    */
   def apply(base: String): URLBuilder = apply(new URL(base))
 }
--- src/test/scala/org/f100ded/scalaurlbuilder/URLBuilderSpec.scala
+++ src/test/scala/org/f100ded/scalaurlbuilder/URLBuilderSpec.scala
@@ -7,78 +7,95 @@ class URLBuilderSpec extends FlatSpec {
   val base = URLBuilder("http://localhost/")
 
   it should "handle bases with and without trailing slash" in {
-    assert(URLBuilder("http://api/v1").addPathSegments("segment").toString() == "http://api/v1/segment")
-    assert(URLBuilder("http://api/v1/").addPathSegments("segment").toString() == "http://api/v1/segment")
+    assert(base.toString == "http://localhost/")
+    assert(URLBuilder("http://api/v1").withPathSegments("segment").toString() == "http://api/v1/segment")
+    assert(URLBuilder("http://api/v1/").withPathSegments("segment").toString() == "http://api/v1/segment")
   }
 
   it should "handle a simple segment" in {
-    assert(base.addPathSegments("segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("segment1").toString() == "http://localhost/segment1")
   }
 
   it should "handle multiple segments" in {
-    assert(base.addPathSegments("seg1/seg2/seg3").toString() == "http://localhost/seg1/seg2/seg3")
+    assert(base.withPathSegments("seg1/seg2/seg3").toString() == "http://localhost/seg1/seg2/seg3")
   }
 
   it should "handle leading-slash segments" in {
-    assert(base.addPathSegments("/").toString() == "http://localhost/")
-    assert(base.addPathSegments("/segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("/").toString() == "http://localhost/")
+    assert(base.withPathSegments("/segment1").toString() == "http://localhost/segment1")
   }
 
   it should "handle trailing-slash segments" in {
-    assert(base.addPathSegments("segment1").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("segment1/").toString() == "http://localhost/segment1/")
+    assert(base.withPathSegments("segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("segment1/").toString() == "http://localhost/segment1/")
   }
 
   it should "handle single-dot and double-dot segments" in {
-    assert(base.addPathSegments("segment1/.").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("segment1/./").toString() == "http://localhost/segment1/")
-    assert(base.addPathSegments("segment1/./segment2").toString() == "http://localhost/segment1/segment2")
-    assert(base.addPathSegments("segment1/segment2/../segment3").toString() == "http://localhost/segment1/segment3")
-    assert(base.addPathSegments("segment1/segment2/..").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("segment1/segment2/../").toString() == "http://localhost/segment1/")
+    assert(base.withPathSegments("segment1/.").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("segment1/./").toString() == "http://localhost/segment1/")
+    assert(base.withPathSegments("segment1/./segment2").toString() == "http://localhost/segment1/segment2")
+    assert(base.withPathSegments("segment1/segment2/../segment3").toString() == "http://localhost/segment1/segment3")
+    assert(base.withPathSegments("segment1/segment2/..").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("segment1/segment2/../").toString() == "http://localhost/segment1/")
   }
 
   it should "handle double-slash segments" in {
-    assert(base.addPathSegments("//segment1").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("///segment1").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("////segment1").toString() == "http://localhost/segment1")
-    assert(base.addPathSegments("segment1//segment2").toString() == "http://localhost/segment1/segment2")
-    assert(base.addPathSegments("segment1///segment2").toString() == "http://localhost/segment1/segment2")
-    assert(base.addPathSegments("segment1////segment2").toString() == "http://localhost/segment1/segment2")
-    assert(base.addPathSegments("segment1/////segment2").toString() == "http://localhost/segment1/segment2")
-    assert(base.addPathSegments("segment1/segment2//").toString() == "http://localhost/segment1/segment2/")
-    assert(base.addPathSegments("segment1/segment2///").toString() == "http://localhost/segment1/segment2/")
-    assert(base.addPathSegments("segment1/segment2////").toString() == "http://localhost/segment1/segment2/")
+    assert(base.withPathSegments("//segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("///segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("////segment1").toString() == "http://localhost/segment1")
+    assert(base.withPathSegments("segment1//segment2").toString() == "http://localhost/segment1/segment2")
+    assert(base.withPathSegments("segment1///segment2").toString() == "http://localhost/segment1/segment2")
+    assert(base.withPathSegments("segment1////segment2").toString() == "http://localhost/segment1/segment2")
+    assert(base.withPathSegments("segment1/////segment2").toString() == "http://localhost/segment1/segment2")
+    assert(base.withPathSegments("segment1/segment2//").toString() == "http://localhost/segment1/segment2/")
+    assert(base.withPathSegments("segment1/segment2///").toString() == "http://localhost/segment1/segment2/")
+    assert(base.withPathSegments("segment1/segment2////").toString() == "http://localhost/segment1/segment2/")
   }
 
   it should "handle a single query parameter" in {
-    assert(base.addQueryParameters("x" -> "1").toString() == "http://localhost/?x=1")
+    assert(base.withQueryParameters("x" -> "1").toString() == "http://localhost/?x=1")
   }
 
   it should "handle multiple query parameters" in {
-    assert(base.addQueryParameters("x" -> "1", "y" -> "2", "z" -> "3").toString() == "http://localhost/?x=1&y=2&z=3")
+    assert(base.withQueryParameters("x" -> "1", "y" -> "2", "z" -> "3").toString() == "http://localhost/?x=1&y=2&z=3")
   }
 
   it should "handle base with query parameters" in {
     val newBase = URLBuilder("http://localhost/segment0?a=1&b=2&c=3")
-    assert(newBase.addQueryParameters("d" -> "4").toString == "http://localhost/segment0?a=1&b=2&c=3&d=4")
-    assert(newBase.addPathSegments("segment1").toString == "http://localhost/segment0/segment1?a=1&b=2&c=3")
-    assert(newBase.addPathSegments("segment1/").toString == "http://localhost/segment0/segment1/?a=1&b=2&c=3")
-    assert(newBase.addPathSegments("seg1/seg2").toString == "http://localhost/segment0/seg1/seg2?a=1&b=2&c=3")
+    assert(newBase.withQueryParameters("d" -> "4").toString == "http://localhost/segment0?a=1&b=2&c=3&d=4")
+    assert(newBase.withPathSegments("segment1").toString == "http://localhost/segment0/segment1?a=1&b=2&c=3")
+    assert(newBase.withPathSegments("segment1/").toString == "http://localhost/segment0/segment1/?a=1&b=2&c=3")
+    assert(newBase.withPathSegments("seg1/seg2").toString == "http://localhost/segment0/seg1/seg2?a=1&b=2&c=3")
   }
 
   it should "handle encoded segments" in {
-    assert(base.addPathSegments("%7Bhello%7D").toString == "http://localhost/%7Bhello%7D")
-    assert(base.addPathSegments("%7Bhello%7D/world/..").toString == "http://localhost/%7Bhello%7D")
+    assert(base.withPathSegments("%7Bhello%7D").toString == "http://localhost/%7Bhello%7D")
+    assert(base.withPathSegments("%7Bhello%7D/world/..").toString == "http://localhost/%7Bhello%7D")
   }
 
   it should "handle url-unsafe segments" in {
-    assert(base.addPathSegments("{hello}/world").toString == "http://localhost/%7Bhello%7D/world")
+    assert(base.withPathSegments("{hello}/world").toString == "http://localhost/%7Bhello%7D/world")
   }
 
   it should "handle url-unsafe parameters" in {
-    assert(base.addQueryParameters("x" -> "http://a/b c/c?x=1").toString ==
+    assert(base.withQueryParameters("x" -> "http://a/b c/c?x=1").toString ==
       "http://localhost/?x=http%3A%2F%2Fa%2Fb+c%2Fc%3Fx%3D1"
     )
+  }
+
+  it should "handle fragments with and without leading #" in {
+    assert(URLBuilder("http://api/v1").withFragment(Some("fragment")).toString() == "http://api/v1#fragment")
+    assert(URLBuilder("http://api/v1").withFragment(Some("#fragment")).toString() == "http://api/v1#fragment")
+    assert(URLBuilder("http://api/v1").withFragment(Some("##fragment")).toString() == "http://api/v1#%23fragment")
+  }
+
+  it should "construct an URL" in {
+    val expectedString = "http://localhost/segment1/segment2/file1#ref1?q1=http%3A%2F%2Fa%2Fb+c%2Fc%3Fx%3D1"
+    val builder = URLBuilder("http://localhost")
+      .withPathSegments("segment1/segment2", "file1")
+      .withQueryParameters("q1" -> "http://a/b c/c?x=1")
+      .withFragment(Some("ref1"))
+
+    assert(builder.toURL.toString == expectedString)
   }
 }
